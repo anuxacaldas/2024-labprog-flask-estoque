98865b5c2de60e595b18f495f77f64857da6a256
ALUNO <aluno@ifsp>
2024-05-15 21:50:56 -0300

    15/05 - Inclusão de lista e icone editar e excluir

--- src/routes/categoria.py
+++ src/routes/categoria.py
@@ -1,5 +1,9 @@
+import flask
 import sqlalchemy as sa
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_required
+
+from src.forms.categoria import NovoCategoriaForm
 from src.models.categoria import Categoria
 from src.modules import db
 
@@ -15,3 +19,20 @@ def lista():
 
     return render_template('categoria/lista.jinja2',
                            rset=rset)
+
+
+@bp.route('/add', methods=['GET', 'POST'])
+@login_required
+def add():
+    form = NovoCategoriaForm()
+    if form.validate_on_submit():
+        categoria = Categoria()
+        categoria.nome = form.nome.data
+        db.session.add(categoria)
+        db.session.commit()
+        flash(f"Categoria '{form.nome.data}' adicionada")
+        return redirect(url_for('categoria.lista'))
+
+    return render_template('categoria/add.jinja2',
+                           title="Nova categoria",
+                           form=form)
