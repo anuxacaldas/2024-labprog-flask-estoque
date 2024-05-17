02ba8adcbd269404f678cd4d0e5ab82341efbf87
ALUNO <aluno@ifsp>
2024-05-16 22:20:53 -0300

    16/05 - Editar e excluir categoria

--- src/forms/categoria.py
+++ src/forms/categoria.py
@@ -9,3 +9,11 @@ class NovoCategoriaForm(FlaskForm):
                            InputRequired(message="É obrigatório informar um nome para a categoria")
                            ])
     submit = SubmitField("Adicionar")
+
+class EditCategoriaForm(FlaskForm):
+    nome = StringField("Nome da categoria",
+                       validators=[
+                           InputRequired(message="É obrigatório informar um nome para a categoria")
+                       ])
+    submit = SubmitField("Alterar")
+
--- src/routes/categoria.py
+++ src/routes/categoria.py
@@ -1,9 +1,8 @@
 import flask
 import sqlalchemy as sa
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, url_for, request
 from flask_login import login_required
-
-from src.forms.categoria import NovoCategoriaForm
+from src.forms.categoria import NovoCategoriaForm, EditCategoriaForm
 from src.models.categoria import Categoria
 from src.modules import db
 
@@ -36,3 +35,40 @@ def add():
     return render_template('categoria/add.jinja2',
                            title="Nova categoria",
                            form=form)
+
+
+@bp.route('/edit/<uuid:id_categoria>', methods=['GET', 'POST'])
+@login_required
+def edit(id_categoria):
+    categoria = Categoria.get_by_id(id_categoria)
+    if categoria is None:
+        flash("Categoria inexistente", category="warning")
+        return redirect(url_for('categoria.lista'))
+
+    form = EditCategoriaForm(request.values, obj=categoria)
+    if form.validate_on_submit():
+        categoria.nome = form.nome.data
+        db.session.commit()
+        flash("Categoria alterada", category="success")
+        return redirect(url_for('categoria.lista'))
+
+    return render_template('categoria/edit.jinja2',
+                           title="Alterar categoria", form=form)
+
+@bp.route('/del/<uuid:id_categoria>', methods=['GET', 'POST'])
+@login_required
+def remove(id_categoria):
+    categoria = Categoria.get_by_id(id_categoria)
+    if categoria is None:
+        flash("Categoria inexistente", category="warning")
+        return redirect(url_for('categoria.lista'))
+
+    db.session.delete(categoria)
+    db.session.commit()
+    flash("Categoria removida", category="success")
+    return redirect(url_for('categoria.lista'))
+
+
+
+
+
