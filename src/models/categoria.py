cf973054a6a51a73af7307cdcf9a6d6bcd9f1e98
ALUNO <aluno@ifsp>
2024-05-23 22:27:16 -0300

    22/05 - Include produto.py, atualizou categoria.py

--- src/models/categoria.py
+++ src/models/categoria.py
@@ -1,10 +1,8 @@
 import uuid
 from sqlalchemy import Uuid, String
-from sqlalchemy.orm import Mapped, mapped_column
-from src.models.base_mixin import (BasicRepositoryMixin,
-                                   TimeStampMixin)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from src.models.base_mixin import (BasicRepositoryMixin, TimeStampMixin)
 from src.modules import db
-
 
 
 class Categoria(db.Model, BasicRepositoryMixin, TimeStampMixin):
@@ -13,6 +11,9 @@ class Categoria(db.Model, BasicRepositoryMixin, TimeStampMixin):
     id: Mapped[Uuid] = mapped_column(Uuid(as_uuid=True),
                                      primary_key=True,
                                      default=uuid.uuid4)
-
     nome: Mapped[str] = mapped_column(String(128),
                                       nullable=False)
+
+    lista_de_produtos = relationship('Produto', back_populates='categoria',
+                                     cascade='all, delete-orphan',
+                                     lazy='select')
